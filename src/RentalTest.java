1cb7a356fdb9703d6e3371e665fe696b9feaefa6
IB <inga@batton.de>
2019-05-01 16:17:56 +0200

    Added tests for Rental

---
+++ src/RentalTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RentalTest {
+    private Movie testMovie;
+    private int daysRented;
+    private Rental testRental;
+
+    @BeforeEach
+    void setUp() {
+        testMovie = new Movie("Testmovie", 0);
+        daysRented = 3;
+        testRental = new Rental(testMovie, daysRented);
+    }
+
+    @Test
+    void getDaysRented() {
+        assertEquals(3, testRental.getDaysRented());
+    }
+
+    @Test
+    void getMovie() {
+        assertTrue(testMovie.getTitle() == testRental.getMovie().getTitle());
+        assertTrue(testMovie.getPriceCode() == testRental.getMovie().getPriceCode());
+    }
+}
